47a7ed10f78431d0f50c9e56cb2aec45660212fe
Curtis <bradleyc8@winthrop.edu>
2025-11-25 15:25:05 -0500

    Printing the entire file

--- PrintingAFile.cpp
+++ PrintingAFile.cpp
@@ -1,5 +1,4 @@
 #include <fstream>
-#include <sstream>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -8,21 +7,26 @@ using namespace std;
 int main() {
         
         string filename;
+	cout << "What is the file's name?: ";
         cin >> filename;
         ifstream inputFile;
         inputFile.open(filename);
+
         if(!inputFile.is_open()){
           cout << "File could not be opened" << endl;
           return(0);
         }
-
-        while(!inputFile.eof()){
-          string line;
-          inputFile >> line;
+	
+	// Was just using to see where the code was messing u
+	//cout << "Checkpoint 1" << endl;
+	
+	string line;	
+        while(getline(inputFile, line)){
           cout << line << endl;
         }
+
         inputFile.close();
-        cout << "Checkpoint" << endl; 
+        //cout << "Checkpoint 2" << endl; 
 
 }
 
